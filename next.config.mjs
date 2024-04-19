2869b71f27e39e3bc446c7e989973e43f508ad0c
Febrianto752 <febrianto.bekasi@gmail.com>
2024-04-19 16:32:57 +0700

    feat: install axios and add images cors url

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["api-bwa-storegg.herokuapp.com"],
+  },
 };
 
 export default nextConfig;
