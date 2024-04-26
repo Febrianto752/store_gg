06e7cb5a5b7f5cf9ca8ca1f56dd39b8e8635795c
Febrianto752 <febrianto.bekasi@gmail.com>
2024-04-26 16:52:21 +0700

    feat: processing create checkout confirmation

--- src/components/organisms/CheckoutConfirmation/index.tsx
+++ src/components/organisms/CheckoutConfirmation/index.tsx
@@ -1,6 +1,44 @@
+import { setCheckout } from "@/services/player";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function CheckoutConfirmation() {
+  const [checkbox, setCheckBox] = useState(false);
+  const router = useRouter();
+
+  const onSubmit = async () => {
+    const dataItemLocal = localStorage.getItem("data-item");
+    const dataTopUpLocal = localStorage.getItem("data-topup");
+
+    const dataItem = JSON.parse(dataItemLocal!);
+    const dataTopUp = JSON.parse(dataTopUpLocal!);
+
+    if (!checkbox) {
+      toast.error("Pastikan anda telah melakukan pembayaran");
+      // [CODE UPDATE] menggagalkan checkout jika checkbox false
+      return;
+    }
+
+    const data = {
+      voucher: dataItem._id,
+      nominal: dataTopUp.nominalItem._id,
+      payment: dataTopUp.paymentItem.payment._id,
+      bank: dataTopUp.paymentItem.bank._id,
+      name: dataTopUp.bankAccountName,
+      accountUser: dataTopUp.verifyID,
+    };
+
+    const response = await setCheckout(data);
+    if (response.error) {
+      toast.error(response.message);
+    } else {
+      // [CODE UPDATE] memindahkan router.push sebelum toast
+      router.push("/complete-checkout");
+      toast.success("Checkout Berhasil");
+    }
+  };
   return (
     <>
       <label className="checkbox-label text-lg color-palette-1">
