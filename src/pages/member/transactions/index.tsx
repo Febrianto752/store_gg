d5e7432af43122aa5a58e65a6750aba3c220d036
Febrianto752 <febrianto.bekasi@gmail.com>
2024-04-29 11:27:36 +0700

    feat: add auth in member overview and transaction dashboard

--- src/pages/member/transactions/index.tsx
+++ src/pages/member/transactions/index.tsx
@@ -37,3 +37,27 @@ export default function Transactions() {
     </>
   );
 }
+
+interface GetServerSideProps {
+  req: {
+    cookies: {
+      token: string;
+    };
+  };
+}
+
+export async function getServerSideProps({ req }: GetServerSideProps) {
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
