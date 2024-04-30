61e7fb8efa3a41a3106a941bec8126859179b547
Febrianto752 <febrianto.bekasi@gmail.com>
2024-04-30 14:32:40 +0700

    feat: add metadata in landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,23 @@ export default function Home() {
   }, []);
   return (
     <>
+      <Head>
+        <title>StoreGG - Get a New Experience in Gaming</title>
+        <meta
+          name="description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta
+          property="og:title"
+          content="StoreGG - Get a New Experience in Gaming"
+        />
+        <meta
+          property="og:description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta property="og:image" content="https://imageurlkalian" />
+        <meta property="og:url" content="https://storegg.com" />
+      </Head>
       <Navbar />
       <MainBanner />
       <TransactionStep />
